175a3928b679f27eb7943448d796f3959471ce5b
jadengoter <jadengoter@huskers.unl.edu>
2021-01-22 01:25:49 -0600

    Added post processing and physics adjustments.

---
+++ Assets/Scripts/GravityMultiplier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityMultiplier : MonoBehaviour
+{
+    [Tooltip("Multiplies the gravity of the rigidbody of this GameObject by the specified multiplier.")]
+    [SerializeField] [Range(1.0f, 50.0f)]
+    private float gravityMultiplier = 2f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(Physics.gravity * (gravityMultiplier - 1f), ForceMode.Acceleration);
+    }
+}
