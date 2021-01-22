1c92ef17158a31fe9c15dbb08141b48e73644765
jadengoter <jadengoter@huskers.unl.edu>
2021-01-22 00:39:00 -0600

    Imported Lux and implemented base movement.

---
+++ Assets/Scripts/TiltMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TiltMovement : MonoBehaviour
+{
+    [Tooltip("Amount the board tilts on input, in degrees.")] [SerializeField] [Range(0.0f, 50.0f)]
+    private float tiltAmount = 15.0f;
+
+    [Tooltip("The board's rotation smoothing speed.")] [SerializeField] [Range(0.0f, 1.0f)]
+    private float smoothSpeed = 0.125f;
+
+    private Vector2 _input;
+
+    private void Update()
+    {
+        // Get input every frame
+        _input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply physics on physics updates
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(_input.y, 0, -_input.x) * tiltAmount);
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            targetRotation,
+            smoothSpeed);
+    }
+}
