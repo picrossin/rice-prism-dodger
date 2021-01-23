019d1f080da74baadc2495c08d03e7ddd78da343
jadengoter <jadengoter@huskers.unl.edu>
2021-01-23 00:24:53 -0600

    Implemented smooth camera following.

---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    [SerializeField] private string playerTag = "Player";
+
+    [Tooltip("The camera's follow smoothing speed.")] [SerializeField] [Range(0.0f, 1.0f)]
+    private float smoothSpeed = 0.125f;
+
+    [Tooltip("The camera's offset position from the player.")] [SerializeField]
+    private Vector3 _offset = new Vector3(0f, 10f, 0f);
+
+    private GameObject _player;
+
+    private void FixedUpdate()
+    {
+        if (_player == null)
+        {
+            _player = GameObject.FindGameObjectWithTag(playerTag);
+        }
+        else
+        {
+            Vector3 targetPosition = _player.transform.position + _offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        }
+    }
+}
